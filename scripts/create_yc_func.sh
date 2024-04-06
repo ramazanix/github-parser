4faee3ceb6fe989b909dcddc547861afbaf12ca8
Ramazan <ramazanui2002@gmail.com>
2024-04-06 12:34:42 +0300

    Created script for creating yc function

---
+++ scripts/create_yc_func.sh
@@ -0,0 +1,15 @@
+zip gh ./gh_parser.py ./requirements.txt
+
+source .env
+
+yc serverless function create --name=gh-parser
+
+yc serverless function version create --function-name=gh-parser \
+  --runtime python312 \
+  --entrypoint gh_parser.handler \
+  --memory 128m \
+  --execution-timeout 600s \
+  --source-path ./gh.zip \
+  --environment GH_TOKEN=$GH_TOKEN,DB_USER=$DB_USER,DB_PASSWORD=$DB_PASSWORD,DB_PORT=$DB_PORT,DB_NAME=$DB_NAME,DB_HOSTNAME=$DB_HOSTNAME
+
+rm gh.zip
