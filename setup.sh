2ac4cd633974988ac02abc31238515e3c7721391
Ramazan <ramazanui2002@gmail.com>
2024-04-06 13:31:28 +0300

    Added missing packages

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo apt-get install zip
+curl -sSL https://storage.yandexcloud.net/yandexcloud-yc/install.sh | bash
+
 python init_db.py && \
 source scripts/create_yc_func.sh && \
 echo "0 * * * * /github_parser/scripts/trigger_yc_func > /dev/null 2>&1" > /etc/crontabs/root
