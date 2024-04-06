3056590238e5a47c900cb0bd31e18b0b5e6e5f42
Ramazan <ramazanui2002@gmail.com>
2024-04-06 14:11:18 +0300

    Fixes

--- setup.sh
+++ setup.sh
@@ -2,9 +2,10 @@
 
 apt-get update -y > /dev/null 2>&1
 apt-get install zip -y > /dev/null 2>&1
-curl -sSL https://storage.yandexcloud.net/yandexcloud-yc/install.sh
+curl -sSL https://storage.yandexcloud.net/yandexcloud-yc/install.sh > /dev/null 2>&1
 source "/root/.bashrc"
 
 python init_db.py && \
 source scripts/create_yc_func.sh && \
 echo "0 * * * * /github_parser/scripts/trigger_yc_func > /dev/null 2>&1" > /etc/crontabs/root
+exec "$@"
