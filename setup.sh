f9d6fcfb3dbb0d8bf0f0485fb1e3f286bfc0e0ee
Ramazan <ramazanui2002@gmail.com>
2024-04-06 12:36:24 +0300

    Changed script path

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 python3 init_db.py && \
 source scripts/create_yc_func.sh && \
-echo "0 * * * * /path/to/script >/dev/null 2>&1" > /etc/crontabs/root
+echo "0 * * * * /github_parser/scripts/trigger_yc_func > /dev/null 2>&1" > /etc/crontabs/root
